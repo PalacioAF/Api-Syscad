35421939d9082899a7b836ae8ba1babf11e683ae
PalacioAF <federicopalacio92@gmail.com>
2024-12-18 21:00:24 -0300

    amend

--- controllers/notificacionesController.js
+++ controllers/notificacionesController.js
@@ -4,7 +4,7 @@ exports.getNotificaciones = async (req, res) => {
     try {
         const notifications = await Notification.find(
           { state: true },
-          { title: 1, description: 1, _id: 0 }
+          { title: 1, description: 1, date:1, _id: 0 }
         );
         res.json({msg:'OK',res:notifications});
     } catch (error) {
